a31441b0acc73e476d4f5d471010b04a66ea6966
Phillip D. Alderman <phillip.alderman@okstate.edu>
2024-05-18 01:27:00 -0500

    Reworking csm_create_stan_dydt() to use ... instead of explicit arguments for parameters or forcings

--- R/csm_create_stan_dydt.R
+++ R/csm_create_stan_dydt.R
@@ -6,99 +6,196 @@
 #' @param states a list vector containing CSM state
 #'   variables defined using [csmdeveloper::csm_create_state()]
 #'   in the intended order
-#' @param parameters an optional list vector containing
-#'    CSM parameters defined using [csmdeveloper::csm_create_parameter()]
-#'    the names of all parameters
-#' @param forcings an optional character vector
-#'    containing the names of all forcing variables
+#' @param arg_names a named character vector whose elements
+#'   correspond to the argument name in the function to be
+#'   generated and whose names correspond to either `states`
+#'   or the name of the argument in `...`
+#' @param ... optional arguments of list vectors containing
+#'   CSM parameters defined using [csmdeveloper::csm_create_parameter()]
+#'   or CSM variables defined using [csmdeveloper::csm_create_variable()]
 #'
 #' @export
-csm_create_stan_dydt <- function(name, states,
-                            parameters = NULL,
-                            forcings = NULL,
-                            state_name = "y",
-                            parameter_name = "theta",
-                            forcing_name = "forcings"){
+csm_create_stan_dydt <- function(name, state, arg_names = c(state = "y"),
+                                 ...){
 
-  state_arg <- paste0("vector ", state_name)
+  dots_list <- list(...)
 
-  if(!is.null(parameters)){
-    parameter_arg <- paste0("vector ", parameter_name)
+  state_type <- get_v_type(state)
+
+  state_arg <- paste0(state_type, " ", arg_names["state"])
+
+  if(length(dots_list) > 0){
+    dots_args <- get_dots_args(dots_list, arg_names)
   }else{
-    parameter_arg <- NULL
+    dots_args <- NULL
   }
 
-  if(!is.null(forcings)){
-    forcings_arg <- paste0("row_vector ", forcings_name)
-  }else{
-    forcings_arg <- NULL
-  }
-
-  dydt_args <- paste0(c(state_arg, parameter_arg, forcings_arg),
+  dydt_args <- paste0(c(state_arg, dots_args),
                       collapse = ", ")
 
-  dydt_signature <- paste0("vector ", name, "(", dydt_args, ")")
+  dydt_signature <- paste0(state_type, " ", name, "(", dydt_args, ")")
 
-  dydt_states <-
+  dydt_state <-
     c("// State variables:",
       mapply(dydt_declaration,
-             .i = 1:length(states),
-             .name = names(states),
-             .var = states,
-             .vec_in = state_name),
+             .i = 1:length(state),
+             .name = names(state),
+             .var = state,
+             .in = arg_names["state"],
+             .in_type = state_type),
       ""
       )
 
-  if(is.null(parameters)){
-    dydt_parameters <- NULL
+  if(length(dots_list) == 0){
+    dydt_dots <- NULL
   }else{
-    dydt_parameters <-
-      c("// Parameters:",
-        mapply(dydt_declaration,
-               .i = 1:length(parameters),
-               .name = names(parameters),
-               .var = parameters,
-               .vec_in = parameter_name),
-        ""
-      )
+    dydt_dots <- get_dydt_dots(dots_list, arg_names)
   }
 
   dydt_rates <-
     c(
       "// Calculation of rate of change for state variables:",
       mapply(dydt_rate_eqs,
-             .i = 1:length(states),
-             .state = states,
-             .state_name = state_name),
+             .i = 1:length(state),
+             .state = state,
+             .state_name = arg_names["state"]),
       ""
     )
 
   dydt_body <- c(
-    dydt_states,
-    dydt_parameters,
+    dydt_state,
+    dydt_dots,
     dydt_rates
   )
 
   dydt_function <- c(
     paste0(dydt_signature, "{"),
+    "",
     paste0("  ", dydt_body),
-    paste0("  return d", state_name, "_dt;"),
-    "}"
+    paste0("  return d", arg_names["state"], "_dt;"),
+    "}",
+    ""
   )
 
   return(dydt_function)
 }
 
-dydt_declaration <- function(.i, .name, .var, .vec_in){
+dydt_declaration <- function(.i, .name, .var, .in, .in_type = "vector"){
+  if(.in_type == "vector"){
+    .in_ind <- paste0("[", .i, "]")
+  }else{
+    .in_ind <- ""
+  }
   c(
     paste0("// ", .name, ": ",
            .var,
            " (", attr(.var, "units"), ")"),
-    paste0("real ", .name, " = ", .vec_in, "[", .i, "];")
+    paste0("real ", .name, " = ", .in, .in_ind, ";")
   )
 }
 
 dydt_rate_eqs <- function(.i, .state, .state_name){
   paste0("d", .state_name, "_dt[", .i, "] = ",
          attr(.state, "equation")[2],";")
+}
+
+get_dots_args <- function(dots_list, arg_names){
+
+  dots_type <-
+    dots_list |>
+    lapply(get_v_type) |>
+    unlist()
+
+  dots_name <-
+    1:length(dots_list) |>
+    lapply(get_arg_name,
+           .v_list = dots_list,
+           arg_names = arg_names) |>
+    unlist()
+
+  dots_args <-
+    paste0(dots_type, " ", dots_name)
+
+  return(dots_args)
+}
+
+get_v_type <- function(.v){
+  if(length(.v) == 1){
+    return("real")
+  }else{
+    return("vector")
+  }
+}
+
+get_arg_name <- function(.i, .v_list, arg_names){
+  if(names(.v_list)[.i] %in% arg_names){
+    .arg_name <- arg_names[names(.v_list)[.i]]
+  }else{
+    .arg_name <- paste0("arg_", .i)
+  }
+  return(.arg_name)
+}
+
+get_declaration <- function(.d, dots_list, arg_names){
+
+  .d_name <- get_arg_name(.d, dots_list, arg_names)
+
+  .d_type <- get_v_type(dots_list[[.d]])
+
+  mapply(dydt_declaration,
+         .i = 1:length(dots_list[[.d]]),
+         .name = names(dots_list[[.d]]),
+         .var = dots_list[[.d]],
+         .in = .d_name,
+         .in_type = .d_type,
+         SIMPLIFY = FALSE) |>
+    lapply(paste0, collapse = "\n")
+}
+
+is_csm_parameter <- function(.v){
+  "csm_parameter" %in% class(.v)
+}
+
+get_dydt_dots <- function(dots_list, arg_names){
+
+  dots_declaration <-
+    1:length(dots_list) |>
+    lapply(get_declaration,
+           dots_list = dots_list,
+           arg_names = arg_names) |>
+    unlist()
+
+  dots_is_parameter <-
+    dots_list |>
+    lapply(\(.x) sapply(.x, is_csm_parameter)) |>
+    unlist()
+
+  if(!any(dots_is_parameter)){
+    dydt_parameters <- NULL
+  }else{
+    dydt_parameters <-
+      dots_is_parameter |>
+      (\(.x) dots_declaration[.x])() |>
+      (\(.x) c("// Parameters:", .x))() |>
+      strsplit(split = "\n") |>
+      unlist()
+  }
+
+  if(all(dots_is_parameter)){
+    dydt_other_variables <- NULL
+  }else{
+    dydt_other_variables <-
+      dots_is_parameter |>
+      (\(.x) dots_declaration[!.x])() |>
+      (\(.x) c("// Other Variables:", .x))() |>
+      strsplit(split = "\n") |>
+      unlist()
+  }
+
+  dydt_dots <- c(dydt_parameters,
+                 "",
+                 dydt_other_variables,
+                 "")
+
+  return(dydt_dots)
 }
